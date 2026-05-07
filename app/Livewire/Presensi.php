0d90cceb52c9089695f250961ccca11498948f53
Rapsannn <rafsanjaniok01@gmail.com>
2026-05-07 09:29:07 +0700

    presensi pitch

---
+++ app/Livewire/Presensi.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Schedule;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class Presensi extends Component
+{
+    public function render()
+    {
+        $schedule = Schedule::where('user_id', Auth::id())->first();
+        return view('livewire.presensi', compact('schedule'))->layout('layouts.main');
+    }
+}
